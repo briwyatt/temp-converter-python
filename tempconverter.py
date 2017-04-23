23af7f7498d0c6db66d16d5453065296e3617ac2
Bri Wyatt <mbriwyatt@gmail.com>
2017-04-22 19:19:54 -0500

    inital commit

---
+++ tempconverter.py
@@ -0,0 +1,8 @@
+# Ask the user for a temperature in Celsius
+celsius = int(input('Temp. in Celsius:    '))
+
+#  Calculate the conversion
+fahrenheit = celsius*9/5 + 32
+
+#   Ouput the converted temperature as a result
+print(fahrenheit, 'Fahrenheit')
